54c6b9e47116d4e3bcc3de5b9a2a7fb0dde6d4e2
Braxton Brown <braxton.brown@outlook.com>
2026-05-04 01:39:36 -0600

    only create log file in debug mode

--- lsp/src/logging.rs
+++ lsp/src/logging.rs
@@ -13,33 +13,34 @@ pub fn setup_logging_to_stderr_and_file(
     let stderr_log_level = filter::LevelFilter::DEBUG;
     #[cfg(not(debug_assertions))]
     let stderr_log_level = filter::LevelFilter::INFO;
-    #[cfg(debug_assertions)]
-    let file_log_level = filter::LevelFilter::DEBUG;
-    #[cfg(not(debug_assertions))]
-    let file_log_level = filter::LevelFilter::ERROR;
     let stderr_layer = tracing_subscriber::fmt::layer().with_writer(io::stderr);
 
-    let file_layer = tracing_subscriber::fmt::layer().with_writer(
-        fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(file_path.as_ref())
-            .context("opening logging file")?,
+    let reg = tracing_subscriber::registry().with(
+        stderr_layer
+            .with_timer(ChronoLocal::rfc_3339())
+            .with_filter(stderr_log_level),
     );
 
-    tracing_subscriber::registry()
-        .with(
-            stderr_layer
-                .with_timer(ChronoLocal::rfc_3339())
-                .with_filter(stderr_log_level),
-        )
-        .with(
+    #[cfg(debug_assertions)]
+    let reg = {
+        let file_log_level = filter::LevelFilter::DEBUG;
+        let file_layer = tracing_subscriber::fmt::layer().with_writer(
+            fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(file_path.as_ref())
+                .context("opening logging file")?,
+        );
+
+        reg.with(
             file_layer
                 .with_timer(ChronoLocal::rfc_3339())
                 .with_ansi(false)
                 .with_filter(file_log_level),
         )
-        .try_init()?;
+    };
+
+    reg.try_init()?;
 
     Ok(())
 }
